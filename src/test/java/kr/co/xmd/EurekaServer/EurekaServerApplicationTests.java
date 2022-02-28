8346725e135c7105968dd93af8cbb9bf77ae24bc
heungsikyu <hsyouatlas@gmail.com>
2022-02-28 16:44:34 +0900

    init

---
+++ src/test/java/kr/co/xmd/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.xmd.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
